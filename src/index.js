12d55af1aa92ca01968478918c206edb1aa37ec1
danieltistoj <jose.tisrey@gmail.com>
2022-02-12 17:06:24 -0600

    index con ejs

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ app.set('view engine','ejs')
 
 app.set('port', process.env.PORT || 3000);//aqui decios que tome el puerto del procesador, si no existe que tome el 3000
 
+//Routes
+app.use('/', require('./routes/index'))
+
 //empezando el servidor
 app.listen(app.get('port'),()=>{
     console.log('Servidor en puerto',app.get('port'))
