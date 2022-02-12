949bad3c7bc3517f34cee57f0e54e678889beb8c
danieltistoj <jose.tisrey@gmail.com>
2022-02-12 15:51:43 -0600

    servidor con ejs-mate

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const engine = require('ejs-mate')
+const path = require('path')
 
 const app = express()
 //configuraciones
-app.set('port', process.env.PORT || 3000)//aqui decios que tome el puerto del procesador, si no existe que tome el 3000
 
- //empezando el servidor
+app.set('views',path.join(__dirname,'views'))//dirname regresa la direccion del archivo
+app.engine('ejs', engine)
+app.set('view engine','ejs')
 
+app.set('port', process.env.PORT || 3000);//aqui decios que tome el puerto del procesador, si no existe que tome el 3000
+
+//empezando el servidor
 app.listen(app.get('port'),()=>{
-    console.log('Server on port',app.get('port'))
+    console.log('Servidor en puerto',app.get('port'))
 })
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router
+
+router.get('/',(req,res,next)=>{
+    res.render('index')
+})
+router.get('/registrarse',(req,res,next)=>{
+
+})
+router.post('/registrarse',(req,res,next)=>{
+
+})
+router.get('/entrar',(req,res,next)=>{
+
+})
+
+module.exports = router //para utilizarlo en otros archivos
